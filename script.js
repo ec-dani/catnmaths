9870c5d77e70761f4573745d936ad5de958fa89f
ec-dani <jd.cantuna@alumnos.upm.es>
2020-10-26 12:10:18 +0100

    api calls done simple web desing"

---
+++ script.js
@@ -0,0 +1,23 @@
+const urlmath = "http://numbersapi.com/random/trivia";
+const urlcat ="https://aws.random.cat/meow?ref=apilist.fun";
+
+//document.getElementById("math").innerHTML= math;
+
+//Catphoto
+  fetch(urlcat).then(res => res.json())
+  .then(data => {
+      console.log(data.file);
+      
+      document.getElementById("catphoto").innerHTML= ` <img src="${data.file}" width="500">`
+
+    }
+     )
+
+//Math quote
+  fetch(urlmath).then( data => data.text())
+    .then(
+      data => {
+        document.getElementById("math").innerHTML = data;
+      }
+  )
+

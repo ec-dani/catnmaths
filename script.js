19c4c4216008deef998277e1272e390e4ea50ba7
ec-dani <jd.cantuna@alumnos.upm.es>
2020-10-26 21:40:46 +0100

    cat errased

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const urlcat ="https://aws.random.cat/meow?ref=apilist.fun";
 
 
 //Catphoto
-  fetch(urlcat).then(res => res.json())
+/*  fetch(urlcat).then(res => res.json())
   .then(data => {
       console.log(data.file);
       
       document.getElementById("catphoto").innerHTML= ` <img src="${data.file}" width="500">`
 
     }
-     );
+     );*/
 
 //Math quote
   fetch(urlmath).then( data => data.text())

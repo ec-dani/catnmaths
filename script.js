700fb23dd46946ff78d5cc6c312e6baab3b11418
ec-dani <jd.cantuna@alumnos.upm.es>
2020-10-27 16:07:40 +0100

    api calls using ajax

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const urlmath = "http://numbersapi.com/random/trivia";
 const urlcat ="https://aws.random.cat/meow?ref=apilist.fun";
 
-
-//Catphoto
-/*  fetch(urlcat).then(res => res.json())
-  .then(data => {
+function showCat(){
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if (this.status==200 && this.readyState ==4 ){
+      let data = JSON.parse(this.responseText);
       console.log(data.file);
-      
-      document.getElementById("catphoto").innerHTML= ` <img src="${data.file}" width="500">`
-
+      document.getElementById("catphoto").innerHTML = ` <img src="${data.file}" width="50%"> `
     }
-     );*/
+  };
+  xhttp.open("GET",urlcat,true);
+  xhttp.send();
+}
 
-//Math quote
-  fetch(urlmath).then( data => data.text())
-    .then(
-      data => {
-        document.getElementById("math").innerHTML = data;
-      }
-  );
+function showMath(){
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if (this.status==200 && this.readyState ==4 ){
+      document.getElementById("math").innerHTML= this.responseText;
+    }
+  };
+  xhttp.open("GET",urlmath,true);
+  xhttp.send();
+}
+showMath();
+showCat();
 

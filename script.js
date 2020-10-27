d39db888061ddcd280a17528605ddba489e84774
ec-dani <jd.cantuna@alumnos.upm.es>
2020-10-27 20:56:12 +0100

    breaking bad quotes added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const urlmath = "http://numbersapi.com/random/trivia";
-const urlcat ="https://aws.random.cat/meow?ref=apilist.fun";
+const urlBbquote = "https://www.breakingbadapi.com/api/quote/random";
+const urlCat ="https://aws.random.cat/meow?ref=apilist.fun";
 
 function showCat(){
   const xhttp = new XMLHttpRequest();
@@ -7,24 +7,25 @@ function showCat(){
     if (this.status==200 && this.readyState ==4 ){
       let data = JSON.parse(this.responseText);
       console.log(data.file);
-      document.getElementById("catphoto").innerHTML = ` <img src="${data.file}" width="50%"> `
+      document.getElementById("catphoto").innerHTML = ` <img src="${data.file}"> `
     }
   };
-  xhttp.open("GET",urlcat,true);
+  xhttp.open("GET",urlCat,true);
   xhttp.send();
 }
 
-function showMath(){
+function showBbquote(){
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if (this.status==200 && this.readyState ==4 ){
-      document.getElementById("math").innerHTML= this.responseText;
-      console.log(this.responseText);
+      let data = JSON.parse(this.responseText);
+      document.getElementById("quote").innerHTML= data[0].quote;
+      document.getElementById("author").innerHTML = data[0].author;
     }
   };
-  xhttp.open("GET",urlmath,true);
+  xhttp.open("GET",urlBbquote,true);
   xhttp.send();
 }
-showMath();
-showCat();
 
+showCat();
+showBbquote();
